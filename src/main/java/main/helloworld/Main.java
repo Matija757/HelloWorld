6fa58a4e57e5e5ee73b7af1dea5ff5af865b392a
Matija <matijaaalosic@gmail.com>
2020-02-04 20:20:33 +0100

    JavaCommit

--- src/main/java/main/helloworld/Main.java
+++ src/main/java/main/helloworld/Main.java
@@ -1,5 +1,7 @@
 package main.helloworld;
 
+import java.util.Scanner;
+
 /**
  *
  * @author qa
@@ -13,6 +15,36 @@ public class Main {
         
         System.out.println("HelloWorld!");
         System.out.println("MatijaLosic");
-    }
+        
+        int age = 5;
+        
+        if (age > 18) {
+            System.out.println("Osoba je punoletna.");
+            } else {
+            System.out.println("Osoba je maloletna");
+        }
+        
+        System.out.print("Unesite broj godina: ");
+        Scanner input = new Scanner(System.in);
+        int userAge = input.nextInt();
+        
+        System.out.println("Korisnik je uneo: " + userAge);
+        
+        if (userAge < 0 && userAge > 150) {
+            System.out.println("Invalid");
+        } else if (userAge > 0 && userAge <= 5) {
+            System.out.println("Baby");
+        } else if (userAge <= 11) {
+            System.out.println("Kid");
+        } else if (userAge <=17) {
+            System.out.println("Teen");
+        } else if (userAge >= 18) {
+            System.out.println("Adult");
+        } 
+        }
+        }
+         
+        
     
-}
+    
+
